e06aeb4eb0c36fc6c6dddf04858fb258bb642402
Dustin Kraft <kraftdr@gmail.com>
2015-02-03 12:13:58 -0700

    Initial Checkin

---
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    public class Program
+    {
+        //this is the number the user needs to guess.  Set its value in your code using a RNG.
+        static int NumberToGuess = 0; 
+       
+        static void Main(string[] args)
+        {
+
+          
+        }
+        
+        public static bool ValidateInput(string userInput)
+        {
+            //check to make sure that the users input is a valid number between 1 and 100.
+            return false;
+        }
+
+        public static void SetNumberToGuess(int number)
+        {
+            //TODO: make this function override your global variable holding the number the user needs to guess.  This is used only for testing methods.
+        }
+
+        public static bool IsGuessTooHigh(int userGuess)
+        {
+            //TODO: return true if the number guessed by the user is too high
+            return false;
+        }
+
+        public static bool IsGuessTooLow(int userGuess)
+        {
+            //TODO: return true if the number guessed by the user is too low
+            return false;
+        }
+    }
+}
---
+++ GuessThatNumber/GuessThatNumber/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GuessThatNumber
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void TestInputValid() 
+        {
+            Assert.IsTrue(Program.ValidateInput("50"), "Valid input returned false");
+        }
+
+        [Test]
+        public void TestInputInvalid()
+        {
+            Assert.IsFalse(Program.ValidateInput("asdf"), "Invalid input returned true");
+        }
+
+        [Test]
+        public void TestTooHigh()
+        {
+            Program.SetNumberToGuess(50);
+            Assert.IsTrue(Program.IsGuessTooHigh(100), "Guess is higher than the number, but program said it was not too high");
+            Assert.IsFalse(Program.IsGuessTooLow(100), "Guess is higher than the number, but program said it was too low");
+        }
+
+        [Test]
+        public void TestTooLow()
+        {
+            Program.SetNumberToGuess(50);
+            Assert.IsFalse(Program.IsGuessTooHigh(1), "Guess is lower than the number, but program said it was too high");
+            Assert.IsTrue(Program.IsGuessTooLow(1), "Guess is lower than the number, but program said it was not too low");
+        }
+
+        [Test]
+        public void TestNumberGuessed()
+        {
+            Program.SetNumberToGuess(50);
+            Assert.IsFalse(Program.IsGuessTooHigh(50), "Guess is the number, but program said it was too high");
+            Assert.IsFalse(Program.IsGuessTooLow(50), "Guess is the number, but program said it was too low");
+        }
+
+    }
+}
